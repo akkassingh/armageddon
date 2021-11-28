9d063629c7c20d2ed77ff04f082fff826ccd5b0c
Ishaank <ishaankhare31@gmail.com>
2021-11-28 20:07:28 +0530

    date added for sort

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 (async function () {
   try {
     // await mongoose.connect(process.env.MONGO_URI, {
-      await mongoose.connect(process.env.MONGO_URI, {
+      await mongoose.connect(process.env.MONGO_URI_dev, {
 
       useNewUrlParser: true,
       useUnifiedTopology: true,
