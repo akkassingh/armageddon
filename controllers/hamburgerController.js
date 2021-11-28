9b8c2f5e4dcafcc17ee8bc2107329a08e555b459
Ishaank <ishaankhare31@gmail.com>
2021-11-28 20:27:46 +0530

    booking in hamburger done

--- controllers/hamburgerController.js
+++ controllers/hamburgerController.js
@@ -3,6 +3,8 @@ const Comment = require("../models/Comment");
 const PostVote = require("../models/PostVote");
 const Animal = require("../models/Animal");
 const ServiceBooking = require("../models/ServiceBooking");
+const {bookingDetails} = require("../models/ServiceBooking");
+const {ServiceAppointment}=require("../models/Service")
 
 module.exports.getBookmarks = async (req, res, next) => {
     const { offset = 0 } = req.params;
@@ -50,6 +52,28 @@ module.exports.getBookmarks = async (req, res, next) => {
           : getTotalCommentsResp[0].totalComments;
     }
     return res.send({"bookmarks": bookmarks});
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.getBookings = async (req, res, next) => {
+  try {
+    let serviceList1=[]
+    let serviceList = await bookingDetails.find({
+      User: res.locals.user._id //recieved=0,accepted(confirmed=1).rejected(cancelled)=2,completed=3
+    }).sort({start:1});
+    for(let i=0;i<serviceList.length;i++){
+      let obj= await ServiceAppointment.findOne({
+        bookingDetails: serviceList[i]._id,
+        bookingStatus:0
+      }).populate('bookingDetails','package run1 run2 startDate dayOff').populate('petDetails', 'name username'); 
+      if(obj!=null)
+      serviceList1.push(obj);
+    }   
+  
+    return res.status(200).json({serviceList:serviceList1});
   } catch (err) {
     console.log(err);
     next(err);
